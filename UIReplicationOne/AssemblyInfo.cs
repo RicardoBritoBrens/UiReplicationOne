554981430ba942f7a257875173ed9100fdef54a1
Ricardo Brito Brens <britobrensricardo@gmail.com>
2021-06-13 17:57:38 -0400

    Initial Commit

---
+++ UIReplicationOne/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("FontAwesome5FreeRegular400.otf", Alias = "FormAwesomeSymbols")]
+[assembly: ExportFont("FontAwesome5Regular400.otf", Alias = "FormAwesomeRegular")]
+[assembly: ExportFont("FontAwesome5Solid900.otf", Alias = "FormAwesomeRegularBold")]
---
+++ UIReplicationOne/Views/WelcomePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UIReplicationOne.ModelViews;
+using Xamarin.Forms;
+
+namespace UIReplicationOne.Views
+{
+    public partial class WelcomePage : ContentPage
+    {
+        public WelcomePage()
+        {
+            BindingContext = new WelcomePageViewModel();
+            InitializeComponent();
+        }
+    }
+}
