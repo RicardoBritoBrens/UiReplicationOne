7fb5e2fa46928b93c1e3567cac11f7bec1bbc4e5
Ricardo Brito Brens <britobrensricardo@gmail.com>
2021-07-01 23:18:25 -0400

    [IMP] finish sign in design an interactions

--- UIReplicationOne/ModelViews/SignInPageViewModel.cs
+++ UIReplicationOne/ModelViews/SignInPageViewModel.cs
@@ -1,26 +1,77 @@
 ﻿using System;
 using System.Windows.Input;
 using Xamarin.Forms;
-
 using System.Threading.Tasks;
 using UIReplicationOne.Services;
+using UIReplicationOne.ModelViews.Base;
 
 namespace UIReplicationOne.ModelViews
 {
-    public class SignInPageViewModel
+    public class SignInPageViewModel : BaseViewModel
     {
         public ICommand FakeSignInCommand { get; set; }
         public ICommand FakeSignUpCommand { get; set; }
         public ICommand FakeForgotPasswordCommand { get; set; }
         public ICommand NavigateBackCommand { get; set; }
         public INavigation _navigation { get; set; }
+
+        public bool UsernameValid
+        {
+            get => _usernameValid;
+            set
+            {
+                _usernameValid = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool PasswordValid
+        {
+            get => _passwordValid;
+            set
+            {
+                _passwordValid = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool _usernameValid;
+        public bool _passwordValid;
+        public string Username
+        {
+            get => _username;
+
+            set
+            {
+                _username = value;
+                OnPropertyChanged();
+                UsernameValid = (!string.IsNullOrWhiteSpace(Username) && Username.Length > 5);
+                ActionEnable = (UsernameValid && PasswordValid);
+            }
+        }
+        private string _username;
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+                PasswordValid = (!string.IsNullOrWhiteSpace(Password) && Password.Length > 5);
+                ActionEnable = (UsernameValid && PasswordValid);
+            }
+        }
+        private string _password;
         public SignInPageViewModel(INavigation navigation)
         {
             FakeSignInCommand = new Command(FakeSignInAsync);
             FakeSignUpCommand = new Command(FakeSignUp);
             FakeForgotPasswordCommand = new Command(ForgotPassowrd);
             NavigateBackCommand = new Command(async () => await NavigateBack());
+            UsernameValid = false;
+            PasswordValid = false;
             _navigation = navigation;
+
         }
 
         private void ForgotPassowrd()
