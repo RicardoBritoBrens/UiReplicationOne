41214c521e5c431b02e5367c4dde2d7a83530bb7
Ricardo Brito Brens <britobrensricardo@gmail.com>
2021-07-01 22:16:04 -0400

    * MessageIOS.cs: * UIReplicationOne.iOS.csproj: * UIReplicationOne.Android.csproj: [IMP]
    * WelcomePageViewModel.cs:
    
    * UIReplicationOne.csproj: [IMP] craete new interface for toast
      messages and implement

--- UIReplicationOne/ModelViews/WelcomePageViewModel.cs
+++ UIReplicationOne/ModelViews/WelcomePageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Input;
+using UIReplicationOne.Services;
 using UIReplicationOne.Views;
 using Xamarin.Forms;
 
@@ -8,11 +9,18 @@ namespace UIReplicationOne.ModelViews
     public class WelcomePageViewModel
     {
         public ICommand NavigateToSignInPageCommand { get; set; }
+        public ICommand FakeCreateAnAccountCommand { get; set; }
         public INavigation _navigation { get; set; }
         public WelcomePageViewModel(INavigation navigation)
         {
             NavigateToSignInPageCommand = new Command(NavigateToSignInPage);
+            FakeCreateAnAccountCommand = new Command(FakeCreateAnAccount);
             _navigation = navigation;
+        }
+
+        private void FakeCreateAnAccount()
+        {
+            DependencyService.Get<IMessage>().LongAlert("Thanks to Create an account Mr/Ms User");
         }
 
         private void NavigateToSignInPage()
